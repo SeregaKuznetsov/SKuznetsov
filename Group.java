6d26bda8e87c6e822e972cf51d5f9a7c809a243f
SeregaKuznetsov <SeregaKuznetsov98@gmail.com>
2016-03-13 09:47:35 +0300

    gropSort
    homework by Shell sort

---
+++ Group.java
@@ -0,0 +1,6 @@
+public class Group {
+    String name;
+    public Group(String name, Student [] students) {
+        this.name = name;
+    }
+}
---
+++ Student.java
@@ -0,0 +1,22 @@
+
+import java.time.LocalDate;
+import java.time.Month;
+
+public class Student {
+
+    private String name;
+    private LocalDate date;
+
+    public Student(String name, int year, Month month, int number) {
+        this.name = name;
+        this.date = LocalDate.of(year, month, number);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+}
