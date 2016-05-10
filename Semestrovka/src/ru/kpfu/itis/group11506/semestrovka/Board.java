223b39770ee9155d430590c951edfc5a51341a54
SeregaKuznetsov <SeregaKuznetsov98@gmail.com>
2016-05-10 20:33:53 +0300

    Update Board.java

--- Semestrovka/src/ru/kpfu/itis/group11506/semestrovka/Board.java
+++ Semestrovka/src/ru/kpfu/itis/group11506/semestrovka/Board.java
@@ -1,68 +1,215 @@
-package ru.kpfu.itis.group11506.semestrovka;
-
 public class Board {
 
-    public static String [][] table = new String[9][9];
-    public final static String empty = "0";
-    public static String initalPoint;
-    public static String endPoint;
-    private int initalRow;
-    private int initalJ;
-    private int endRow;
-    private int endJ;
+    private static String[][] table;
+    private static String inputBoard;
+    private static int size;
+    private static int startI;
+    private static int startJ;
+    private static int endI;
+    private static int endJ;
 
-    public Board(String initalPoint, String endPoint) {
-        createTable();
-        setInitalPoint(initalPoint);
-        setEndPoint(endPoint);
-        addPoint();
+    public Board(String inputBoard) {
+        this.inputBoard = inputBoard;
+        table = new String[size][size];
     }
 
-    public String[][] getTable() {
+    public static void setSize(int initSize) {
+        size = initSize;
+    }
+
+    public String[][] getBoard() {
         return table;
     }
 
-    private void addPoint() {
-        initalRow = (table.length-1) - Character.getNumericValue(initalPoint.charAt(1));
-        initalJ = (int)(initalPoint.charAt(0))-97+1;
-        endRow = (table.length-1) - Character.getNumericValue(endPoint.charAt(1));
-        endJ = (int)(endPoint.charAt(0))-97+1;
-        table[initalRow][initalJ] = Main.figure.getIcon();
-        table[endRow][endJ] = "*";
+    public void createTable() {
+        int symbol = 0;
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                table[i][j] = inputBoard.substring(symbol, symbol+1);
+                symbol++;
+            }
+            symbol+=2;
+        }
     }
 
-    public static void addPathPoint(String point, int number) {
-        int pointRow = (table.length-1) - Character.getNumericValue(point.charAt(1));
-        int pointJ = (int)(point.charAt(0))-97+1;
-        table[pointRow][pointJ] = Integer.toString(number);
-    }
-
-    private void setInitalPoint(String initalPoint) {
-        this.initalPoint = initalPoint;
-    }
-
-    private void setEndPoint(String endPoint) {
-        this.endPoint = endPoint;
-    }
-
-    private void createTable() {
-        for (int i = 0; i < table.length; i++) {
-            for (int j = 0; j < table.length; j++) {
-                table[i][j] = empty;
-                table[8][j] = String.valueOf(((char)(64+j)));
-                table[i][0] = Integer.toString(table.length - (i+1));
+    public void setPoints() {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if (!table[i][j].equals("*") && !table[i][j].equals("-") && !table[i][j].equals("+")) {
+                    startI = i;
+                    startJ = j;
+                }
+                if (table[i][j].equals("+")) {
+                    endI = i;
+                    endJ = j;
+                }
             }
         }
-        table[8][0] = " ";
     }
 
-    public void showTable() {
-        System.out.println();
-        for (int i = 0; i < table.length; i++) {
-            for (int j = 0; j < table.length; j++) {
-                System.out.print(table[i][j]+" ");
-            }
-            System.out.println();
+    public static void showTable(String[][] board) {
+        String empty = "         ";
+        System.out.print("\n   -");
+        for (int u = 0; u < size - 1; u++) {
+            System.out.print("----------");
         }
+        System.out.print("--------\n");
+        boolean number = false;
+        for (int i = 0; i < size; i++) {
+
+
+            System.out.print("  ");
+            for (int m = 0; m < size + 1; m++) {
+                if (m < size) {
+                    if (board[i][m] != null) {
+                        try {
+                            Integer.valueOf(board[i][m]);
+                            number = true;
+                        } catch (NumberFormatException ignored) {
+                        }
+                        if (board[i][m].equals("K")) {
+                            System.out.print("|" + "\033[30m" + "   \\+/" + empty.substring(6) + "\033[0m");
+                        } else if (board[i][m].equals("Q")) {
+                            System.out.print("|" + "\033[30m" + "   \\^/" + empty.substring(6) + "\033[0m");
+                        } else if (board[i][m].equals("R")) {
+                            System.out.print("|" + "\033[30m" + "   (V)" + empty.substring(6) + "\033[0m");
+                        } else if (board[i][m].equals("N")) {
+                            System.out.print("|" + "\033[30m" + "   (\"\\" + empty.substring(6) + "\033[0m");
+                        } else if (board[i][m].equals("B")) {
+                            System.out.print("|" + "\033[30m" + "   [-]" + empty.substring(6) + "\033[0m");
+                        } else if (board[i][m].equals("P")) {
+                            System.out.print("|" + "\033[30m" + "    _" + empty.substring(5) + "\033[0m");
+                        } else if (board[i][m].equals("!")) {
+                            System.out.print("|" + "\033[42m" + empty + "\033[0m");
+                        } else if (number) {
+                            System.out.print("|" + "\033[40m" + empty + "\033[0m");
+                        } else if (board[i][m].equals("+")) {
+                            System.out.print("|" + "\033[33m" + "  o___" + empty.substring(6) + "\033[0m");
+                        } else if (board[i][m].equals("-")) {
+                            System.out.print("|" + "\033[31m" + "    _" + empty.substring(5) + "\033[0m");
+                        } else if (board[i][m].equals("*")) {
+                            System.out.print("|" + empty);
+                        } else {
+                            System.out.print("|" + empty);
+                        }
+                    } else {
+                        System.out.print("|" + empty);
+                    }
+                }
+                number = false;
+            }
+            System.out.println("|");
+
+
+            System.out.print(size - i + " ");
+            for (int j = 0; j < size; j++) {
+                if (board[i][j] != null) {
+                    try {
+                        Integer.valueOf(board[i][j]);
+                        number = true;
+                    } catch (NumberFormatException ignored) {
+                    }
+                    if (board[i][j].equals("K")) {
+                        System.out.print("|" + "\033[30m" + "   ) (" + empty.substring(6) + "\033[0m");
+                    } else if (board[i][j].equals("Q")) {
+                        System.out.print("|" + "\033[30m" + "   ) (" + empty.substring(6) + "\033[0m");
+                    } else if (board[i][j].equals("R")) {
+                        System.out.print("|" + "\033[30m" + "   ) (" + empty.substring(6) + "\033[0m");
+                    } else if (board[i][j].equals("N")) {
+                        System.out.print("|" + "\033[30m" + "   ) '" + empty.substring(6) + "\033[0m");
+                    } else if (board[i][j].equals("B")) {
+                        System.out.print("|" + "\033[30m" + "   | |" + empty.substring(6) + "\033[0m");
+                    } else if (board[i][j].equals("P")) {
+                        System.out.print("|" + "\033[30m" + "   ( )" + empty.substring(6) + "\033[0m");
+                    } else if (board[i][j].equals("!")) {
+                        System.out.print("|" + "\033[42m" + empty + "\033[0m");
+                    } else if (number) {
+                        System.out.print("|" + "\033[40m" + empty + "\033[0m");
+                    } else if (board[i][j].equals("+")) {
+                        System.out.print("|" + "\033[33m" + "  |___|" + empty.substring(7) + "\033[0m");
+                    } else if (board[i][j].equals("-")) {
+                        System.out.print("|" + "\033[31m" + "   ( )" + empty.substring(6) + "\033[0m");
+                    } else if (board[i][j].equals("*")) {
+                    System.out.print("|" + empty);
+                    } else {
+                        System.out.print("|" + empty);
+                    }
+                } else {
+                    System.out.print("|" + empty);
+                }
+                number = false;
+            }
+            System.out.println("|");
+
+
+            System.out.print("  ");
+            for (int m = 0; m < size + 1; m++) {
+                if (m < size) {
+                    if (board[i][m] != null) {
+                        try {
+                            Integer.valueOf(board[i][m]);
+                            number = true;
+                        } catch (NumberFormatException ignored) {
+                        }
+                        if (board[i][m].equals("K")) {
+                            System.out.print("|" + "\033[30m" + "  /___\\" + empty.substring(7) + "\033[0m");
+                        } else if (board[i][m].equals("Q")) {
+                            System.out.print("|" + "\033[30m" + "  /___\\" + empty.substring(7) + "\033[0m");
+                        } else if (board[i][m].equals("R")) {
+                            System.out.print("|" + "\033[30m" + "  /___\\" + empty.substring(7) + "\033[0m");
+                        } else if (board[i][m].equals("N")) {
+                            System.out.print("|" + "\033[30m" + "  /___\\" + empty.substring(7) + "\033[0m");
+                        } else if (board[i][m].equals("B")) {
+                            System.out.print("|" + "\033[30m" + "  /___\\" + empty.substring(7) + "\033[0m");
+                        } else if (board[i][m].equals("P")) {
+                            System.out.print("|" + "\033[30m" + "  /___\\" + empty.substring(7) + "\033[0m");
+                        } else if (board[i][m].equals("!")) {
+                            System.out.print("|" + "\033[42m" + empty + "\033[0m");
+                        } else if (number) {
+                            System.out.print("|" + "\033[40m" + empty + "\033[0m");
+                        } else if (board[i][m].equals("+")) {
+                            System.out.print("|" + "\033[33m" + "  |" + empty.substring(3) + "\033[0m");
+                        } else if (board[i][m].equals("-")) {
+                            System.out.print("|" + "\033[31m" + "  /___\\" + empty.substring(7) + "\033[0m");
+                        } else if (board[i][m].equals("*")) {
+                            System.out.print("|" + empty);
+                        } else {
+                            System.out.print("|" + empty);
+                        }
+                    } else {
+                        System.out.print("|" + empty);
+                    }
+                }
+                number = false;
+            }
+            System.out.println("|");
+
+
+            System.out.print("   -");
+            for (int d = 0; d < size - 1; d++) {
+                System.out.print("----------");
+            }
+            System.out.print("--------\n");
+        }
+        for (int c = 0; c < size; c++) {
+            System.out.print(empty.substring(1) + (char) (c + 65) + " ");
+        }
+        System.out.println("\n");
+    }
+
+    public static int getStartI() {
+        return startI;
+    }
+
+    public static int getStartJ() {
+        return startJ;
+    }
+
+    public static int getEndI() {
+        return endI;
+    }
+
+    public static int getEndJ() {
+        return endJ;
     }
 }
